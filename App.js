2d0fbfd26c709710206cbdbf42d63491077eb5ad
Senior React & Blockchain Dev <40970351+cocadev@users.noreply.github.com>
2021-12-20 10:05:09 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React, { PureComponent } from 'react';
-import Router from './src/router';
+import React, { PureComponent } from 'react'
+import Router from './src/router'
 
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 
-import { configureStore } from './src/store';
-const store = configureStore();
+import { configureStore } from './src/store'
+const store = configureStore()
 
 export default class Driver extends PureComponent {
   render() {
